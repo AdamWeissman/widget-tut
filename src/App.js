733fdce111032d0a44149b56c5189c67c0491c7f
AdamWeissman <adam@comedytragedyepic.com>
2020-07-17 12:45:27 -0400

    basic setup for widget tutorial app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const items = [
+  { 
+    title: "What is React?",
+    content: "React is a front end JS framework"
+  },
+  { 
+    title: "Why React?",
+    content: "It's a favorite"
+  },
+  { 
+    title: "How do you use it?",
+    content: "With component"
+  }
+]
+
+
+export default () => {
+  return(
+    <div>
+      <Accordion items={items} />
+    </div>)
+};
